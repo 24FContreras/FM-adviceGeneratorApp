e67c5154f4e962538eb4620e5427e7dcdbd4fcd8
Franco Contreras <24.f.contreras@gmail.com>
2022-12-18 12:00:30 -0300

    final version of the challenge: -edited README file -added correct link on html footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,12 @@ const adviceBtn = document.querySelector("#randomizeBtn");
 const adviceID = document.querySelector("#adviceID");
 const advice = document.querySelector("#advice");
 
-const handleText = (dataset) => {
-  adviceID.textContent = dataset.slip.id;
-  advice.textContent = dataset.slip.advice;
-};
-
 const fetchAdvice = async () => {
   const res = await fetch("https://api.adviceslip.com/advice");
   const data = await res.json();
 
-  handleText(data);
+  adviceID.textContent = data.slip.id;
+  advice.textContent = data.slip.advice;
 };
 
 adviceBtn.addEventListener("click", () => {
